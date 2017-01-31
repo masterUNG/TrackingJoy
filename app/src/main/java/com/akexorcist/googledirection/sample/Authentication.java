5421531c3a30057ceb07c375bf69e2cb81867e47
masterUNG <phrombutr@gmail.com>
2017-01-31 14:04:36 +0700

    Initial commit

---
+++ app/src/main/java/com/akexorcist/googledirection/sample/Authentication.java
@@ -0,0 +1,13 @@
+package com.akexorcist.googledirection.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Authentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
